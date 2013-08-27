20f344af2a80128debfccd1d68b63c0fab969d93
yllan <yllan@me.com>
2013-08-27 16:50:09 +0800

    Remove compiler warning

--- src/main/scala/utilities/ConfigWrapper.scala
+++ src/main/scala/utilities/ConfigWrapper.scala
@@ -2,6 +2,7 @@ package cc.hypo.utilities
 
 import com.typesafe.config._
 import scala.collection.JavaConverters._
+import scala.language.implicitConversions
 
 class ConfigWrapper(val config: Config) {
   def stringOpt(path: String): Option[String] = {
