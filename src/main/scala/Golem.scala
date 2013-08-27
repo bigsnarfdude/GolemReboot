6c761af33db1fddba10f352bf18b4199b91b712a
yllan <yllan@me.com>
2013-08-27 15:13:40 +0800

    Add node-type configuration.

--- src/main/scala/Golem.scala
+++ src/main/scala/Golem.scala
@@ -12,23 +12,24 @@ object Golem extends App {
   
   val system = ActorSystem("Golem")
 
-  val powerups = conf.getObjectList("powerups").asScala.map((powerupConfig: ConfigObject) => {
-    val clazz = Class.forName(powerupConfig.get("class").unwrapped().asInstanceOf[String])
-    val powerupName = powerupConfig.get("actorName").unwrapped().asInstanceOf[String]
-    system.actorOf(Props(clazz), powerupName)
-  })
+  if (conf.getString("node-type") == "master") {
+    val powerups = conf.getObjectList("powerups").asScala.map((powerupConfig: ConfigObject) => {
+      val clazz = Class.forName(powerupConfig.get("class").unwrapped().asInstanceOf[String])
+      val powerupName = powerupConfig.get("actorName").unwrapped().asInstanceOf[String]
+      system.actorOf(Props(clazz), powerupName)
+    })
 
-  val hipchatUsername = conf.getString("hipchat-username")
-  val hipchatPassword = conf.getString("hipchat-password")
-  val hipchatNickname = conf.getString("hipchat-nickname")
-  val hipchatAuthToken = conf.getString("hipchat-auth-token")
-  val hipchatRooms = conf.getObjectList("hipchat-rooms").asScala
+    val hipchatUsername = conf.getString("hipchat-username")
+    val hipchatPassword = conf.getString("hipchat-password")
+    val hipchatNickname = conf.getString("hipchat-nickname")
+    val hipchatAuthToken = conf.getString("hipchat-auth-token")
+    val hipchatRooms = conf.getObjectList("hipchat-rooms").asScala
 
-  val bot = new Bot(hipchatUsername, hipchatPassword, hipchatNickname, Hipchat(hipchatAuthToken))
-  hipchatRooms.foreach((roomConfig: ConfigObject) =>
-    bot.joinRoom(roomConfig.get("room").unwrapped.asInstanceOf[String],
-                 system.actorSelection(roomConfig.get("handler").unwrapped.asInstanceOf[String]))
-  )
-
+    val bot = new Bot(hipchatUsername, hipchatPassword, hipchatNickname, Hipchat(hipchatAuthToken))
+    hipchatRooms.foreach((roomConfig: ConfigObject) =>
+      bot.joinRoom(roomConfig.get("room").unwrapped.asInstanceOf[String],
+        system.actorSelection(roomConfig.get("handler").unwrapped.asInstanceOf[String]))
+    )
+  }
 }
 
