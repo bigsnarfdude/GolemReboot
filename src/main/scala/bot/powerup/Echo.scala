bad1e7ece752218db46b7b5c7c967383dc0de065
bigsnarfdude <ohprecio@gmail.com>
2014-10-25 08:31:03 -0700

    added ten codes

--- src/main/scala/bot/powerup/Echo.scala
+++ src/main/scala/bot/powerup/Echo.scala
@@ -6,20 +6,24 @@ import cc.hypo.golem.bot.hipchat._
 class Echo extends Actor {
   def receive = {
 
-    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("echo ") => {
-      speaker ! (msg.bodyWithoutFirstMention.substring(5))
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("10-11") => {
+      speaker ! (msg.help)
     }
 
-    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("google ") => {
-      speaker ! (msg.bodyWithoutFirstMention.substring(7))
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("10-28") => {
+      speaker ! (msg.help)
     }
 
-    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("dns ") => {
-      speaker ! (msg.bodyWithoutFirstMention.substring(4))
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("10-33") => {
+      speaker ! (msg.help)
     }
 
-    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("emo ") => {
-      speaker ! (msg.bodyWithoutFirstMention.substring(4))
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("10-99") => {
+      speaker ! (msg.help)
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("checklist hacked") => {
+      speaker ! (msg.help)
     }
 
     case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("command scp get") => {
@@ -34,9 +38,68 @@ class Echo extends Actor {
       speaker ! (msg.sshExample)
     }
 
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("dns ") => {
+      speaker ! (msg.bodyWithoutFirstMention.substring(4))
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("echo ") => {
+      speaker ! (msg.bodyWithoutFirstMention.substring(5))
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("emo ") => {
+      speaker ! (msg.bodyWithoutFirstMention.substring(4))
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("gif ") => {
+      speaker ! (msg.help)
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("google ") => {
+      speaker ! (msg.bodyWithoutFirstMention.substring(7))
+    }
+
     case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("help") => {
       speaker ! (msg.help)
     }
 
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("image") => {
+      speaker ! (msg.help)
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("checklist hacked") => {
+      speaker ! (msg.help)
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("image") => {
+      speaker ! (msg.help)
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("image") => {
+      speaker ! (msg.help)
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("image") => {
+      speaker ! (msg.help)
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("image") => {
+      speaker ! (msg.help)
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("image") => {
+      speaker ! (msg.help)
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("image") => {
+      speaker ! (msg.help)
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("image") => {
+      speaker ! (msg.help)
+    }
+
+    case Asked(msg, speaker) if msg.bodyWithoutFirstMention.startsWith("image") => {
+      speaker ! (msg.help)
+    }
   }
 }
